20b895bd4022fed1fb6b387f6482ea54b661a6fd
Povilas Makackas <povilas.makackas@gmail.com>
2024-07-19 21:27:54 +0300

    added storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-
+import { StorageManager } from '@aws-amplify/ui-react-storage';
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
@@ -37,6 +37,13 @@ function App() {
           <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content} </li>
         ))}
       </ul>
+      <StorageManager
+  acceptedFileTypes={['image/*']}
+  path={({ identityId }) => `protected/${identityId}/`}
+  maxFileCount={1}
+  isResumable
+/>
+
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
